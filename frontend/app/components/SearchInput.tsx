d426866f7bb487993ae844d1b7a97b4431a99ef3
Jen Mak <jennifer.michelle.mak@gmail.com>
2025-09-19 09:53:45 -0400

    Loading state for search input complete

--- frontend/app/components/SearchInput.tsx
+++ frontend/app/components/SearchInput.tsx
@@ -6,16 +6,16 @@ import { useForm, useFormState } from "react-hook-form"
 
 type SearchInputProps = {
   onSearch: (searchTerm: string) => void
-  isLoading?: boolean
   error?: Error | null
   className?: string
   backgroundColor?: string
 }
 
-export function SearchInput({ onSearch, isLoading, className, backgroundColor }: SearchInputProps) {
+export function SearchInput({ onSearch, className, backgroundColor }: SearchInputProps) {
   // Hooks.
   const { handleSubmit, control } = useForm()
-  const { isSubmitting } = useFormState({ control})
+  const { isSubmitting } = useFormState({ control })
+  const [isLoading, setIsLoading] = useState(false)
 
   // Local state.
   const [searchTerm, setSearchTerm] = useState("")
@@ -23,6 +23,7 @@ export function SearchInput({ onSearch, isLoading, className, backgroundColor }:
   // Handlers.
   const handleSearch = () => {
     if (searchTerm.trim()) {
+      setIsLoading(true)
       onSearch(searchTerm.trim())
     }
   }
@@ -35,7 +36,7 @@ export function SearchInput({ onSearch, isLoading, className, backgroundColor }:
 
   return (
     <form onSubmit={handleSubmit(handleSearch)}>
-      <div className={`input-container input-container-loading ${className}`}>
+      <div className={`input-container ${isLoading || isSubmitting ? "input-container-loading" : ""} ${className}`}>
         <Input
           className={`search-input focus-visible:ring-0 ${backgroundColor ? `bg-[${backgroundColor}]` : "bg-transparent"}`}
           placeholder="ie. Gotham"
@@ -45,7 +46,7 @@ export function SearchInput({ onSearch, isLoading, className, backgroundColor }:
         />
         <Button 
           type="submit"
-          disabled={(isLoading || isSubmitting) || !searchTerm.trim()} 
+          disabled={isLoading || isSubmitting || !searchTerm.trim()} 
           className="search-button"
         >
           <ChevronRight className="h-4 w-4" />
